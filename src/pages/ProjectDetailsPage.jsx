287e3359697c0d1b84a0b769fe6e6dadba018954
Julia Solias <juliasohu@gmail.com>
2025-02-12 15:01:26 +0100

    Deleting functionality

--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,40 +1,54 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { API_URL } from "../config/api";
-import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Loader from "../components/Loader";
 
-function ProjectDetailsPage(){
+function ProjectDetailsPage() {
 
     const [project, setProject] = useState(null);
-    const {projectId} = useParams()
+
+    const { projectId } = useParams();
+
+    const navigate = useNavigate();
 
     useEffect(() => {
-       axios.get(`${API_URL}/projects/${projectId}`)
-       .then((response) => {
-        setProject(response.data)
-       })
-       .catch(e => console.log("Errooor", e))
+        axios.get(`${API_URL}/projects/${projectId}`)
+            .then(response => {
+                setProject(response.data);
+            })
+            .catch((error) => console.log("Error getting project details from the API...", error));
     }, []);
 
-    if(project === null){
+
+    const deleteProject = () => {
+        axios.delete(`${API_URL}/projects/${projectId}`)
+            .then( response => {                
+                navigate("/projects");
+            })
+            .catch((error) => console.log("Error deleting project...", error));
+    }
+
+
+    if(project === null) {
         return <Loader />
     }
+
 
     return (
         <div className="ProjectDetailsPage">
             <h1>{project.title}</h1>
             <p>{project.description}</p>
 
-            
             <Link to="/projects">
                 <button>Back to projects</button>
             </Link>
 
-            
             <Link to={`/projects/edit/${projectId}`}>
                 <button>Edit</button>
             </Link>
+
+            <button onClick={deleteProject}>Delete</button>
         </div>
     );
 }
