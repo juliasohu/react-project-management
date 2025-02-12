71cdfcbe61e2dfc022a56b97469290a1ea3f11a6
Julia Solias <juliasohu@gmail.com>
2025-02-12 10:38:37 +0100

    Configuration API_URL

---
+++ src/config/api.js
@@ -0,0 +1,2 @@
+
+export const API_URL = "https://project-management-api-4641927fee65.herokuapp.com"
--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import Loader from "../components/Loader"
+import {API_URL} from "../config/api"
 
 function ProjectListPage(){
 
     const [projects, setProjects] = useState(null)
 
     useEffect(() => {
-        axios.get("https://project-management-api-4641927fee65.herokuapp.com/projects")
+        axios.get(`${API_URL}/projects`)
         .then((response) => {
             setProjects(response.data)
         })
-        .catch(e => console.log("Error getting data..."))
+        .catch(e => console.log("Error getting data...", e))
     }, [])
 
         if (projects === null){
@@ -20,6 +21,15 @@ function ProjectListPage(){
     return (
         <div>
         <h1>Number of projects... {projects.length}</h1>
+
+        {projects.map((p) => {
+            return (
+            <div className="card" key={p.id}>
+            <h2>{p.title}</h2>
+            </div>
+            )
+        })}
+
         </div>
     )
 }
