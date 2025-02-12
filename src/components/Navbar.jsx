1fbe79fc5ae9cffb00e60ccd0a9b90779e613227
Julia Solias <juliasohu@gmail.com>
2025-02-12 11:53:30 +0100

    Implement navbar and styled Loader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import { NavLink } from "react-router-dom";
+
 function Navbar(){
     return (
-        <h1>Welcome to Project Management App</h1>
+        <nav className="Navbar">
+            <NavLink to="/">
+                <button>Home</button>
+            </NavLink>
+
+            <NavLink to="/projects">
+                <button>Projects</button>
+            </NavLink>
+
+            <NavLink to="/projects/create">
+                <button>Create Project</button>
+            </NavLink>
+        </nav>
     );
 }
 
