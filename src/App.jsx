cb497de489e94aa1ee862a30240eb9704d63d0c7
Julia Solias <juliasohu@gmail.com>
2025-02-12 09:38:36 +0100

    Routing and pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
+function App() {
   return (
     <>
-      <h1>Welcome to Project Management App</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<p>Project List Page</p>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar(){
+    return (
+        <h1>Welcome to Project Management App</h1>
+    );
+}
+
+export default Navbar
